33164382c3d86e0e026cb4f2bb29d27596dd1059
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-06-25 10:24:36 +0800

    Quickie with Aries

--- src/app/components/category-create/category-create.component.ts
+++ src/app/components/category-create/category-create.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-category-create',
-  standalone: true,
-  imports: [],
   templateUrl: './category-create.component.html',
-  styleUrl: './category-create.component.css'
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  styleUrls: ['./category-create.component.css']
 })
 export class CategoryCreateComponent {
+  categoryForm: FormGroup;
+  submitted = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private categoryService: CategoryService
+  ) {
+    this.categoryForm = this.formBuilder.group({
+      category_Name: ['', Validators.required],
+      description: ['']
+    });
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.categoryForm.invalid) {
+      return;
+    }
+
+    const category: Category = this.categoryForm.value;
+    this.categoryService.createCategory(category).subscribe(() => {
+      this.router.navigate(['/category-index']);
+    });
+  }
 }
